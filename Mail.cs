07a5d64544e0a58121b064abb18309677da07332
DronTS <109414199+DronTC@users.noreply.github.com>
2023-12-16 00:17:01 +0300

    Добавьте файлы проекта.

---
+++ Mail.cs
@@ -0,0 +1,29 @@
+﻿using System.Net;
+using System.Net.Mail;
+
+namespace MyEmailClient
+{
+    public class Mail
+    {
+        public static MailMessage CreateMail(string fromName, string email, string emailTo, string subject, string body)
+        {
+            var from = new MailAddress(email, fromName);
+            var to = new MailAddress(emailTo);
+            var mail = new MailMessage(from, to);
+
+            mail.Subject = subject;
+            mail.Body = body;
+            mail.IsBodyHtml = true;
+            return mail;
+        }
+
+        public static void SendMail(string host, int smtpPort, string emailFrom, string pass, MailMessage mail)
+        {
+            SmtpClient smtp = new SmtpClient(host, smtpPort);
+            smtp.Credentials = new NetworkCredential(emailFrom, pass);
+            smtp.EnableSsl = true;
+
+            smtp.Send(mail);
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Net;
+using System.Net.Mail;
+
+namespace MyEmailClient
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MailMessage(object sender, RoutedEventArgs e)
+        {
+            string server = textBoxServer.Text;
+            int port = Convert.ToInt32(TextBoxPort.Text);
+            string email = TextBoxEmail.Text;
+            string password = TextBoxPassword.Text;
+            string from = TextBoxFrom.Text;
+            string to = TextBoxTo.Text;
+            string theme = TextBoxTheme.Text;
+            string message = TextBoxDescription.Text;
+
+            if (server == "" || port == 0 || email == "" || password == "" || from == "" || to == "" || theme == "" || message == "")
+            {
+                MessageBox.Show("Необходимо заполнить все поля");
+                return;
+            }
+
+            MailMessage mail = Mail.CreateMail(from, email, to, theme, message);
+
+            Mail.SendMail(server, port, email, password, mail);
+            MessageBox.Show("Сообщение отправлено");
+        }
+    }
+}
